e0777ac8b88f05d326e752fac0a9c6d425498930
ShadowBytess <shadowbyte0321@gmail.com>
2026-08-22 12:37:28 -0400

    chore: ignore native example build artifacts

--- examples/plugins/native-example/target/release/deps/katnip_api-044bbe584f3b36bd.d
+++
@@ -1,7 +0,0 @@
-/home/_lumi/Projects/Katnip-wm/examples/plugins/native-example/target/release/deps/katnip_api-044bbe584f3b36bd.d: /home/_lumi/Projects/Katnip-wm/crates/katnip-api/src/lib.rs
-
-/home/_lumi/Projects/Katnip-wm/examples/plugins/native-example/target/release/deps/libkatnip_api-044bbe584f3b36bd.rlib: /home/_lumi/Projects/Katnip-wm/crates/katnip-api/src/lib.rs
-
-/home/_lumi/Projects/Katnip-wm/examples/plugins/native-example/target/release/deps/libkatnip_api-044bbe584f3b36bd.rmeta: /home/_lumi/Projects/Katnip-wm/crates/katnip-api/src/lib.rs
-
-/home/_lumi/Projects/Katnip-wm/crates/katnip-api/src/lib.rs:
--- examples/plugins/native-example/target/release/deps/katnip_native_example.d
+++
@@ -1,5 +0,0 @@
-/home/_lumi/Projects/Katnip-wm/examples/plugins/native-example/target/release/deps/katnip_native_example.d: src/lib.rs
-
-/home/_lumi/Projects/Katnip-wm/examples/plugins/native-example/target/release/deps/libkatnip_native_example.so: src/lib.rs
-
-src/lib.rs:
